614cee2652ab2631b51ce460fab41937688e22cf
Emmanuel <emmanuel@MacBook-Pro-de-Emmanuel.local>
2020-09-30 20:57:04 +0200

    done

---
+++ components/ProductDetails.js
@@ -0,0 +1,13 @@
+app.component('product-details', {
+  props: {
+    details : {
+      type : Array,
+      required : true
+    }
+  },
+  template:
+  /*html*/
+  `<ul>
+    <li v-for="detail in this.details">{{ detail }}</li>
+  </ul>`
+});
